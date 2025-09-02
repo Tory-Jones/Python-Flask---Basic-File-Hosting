8c0b030bbee58b7fdfcc7cf8df5576ac3ab99eb7
Tory Jones <toryjjones2007@gmail.com>
2025-09-02 10:32:41 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, send_file, url_for
 from flask_autoindex import AutoIndex
 
-app = Flask('Servicing', template_folder='.')
+app = Flask(__name__, template_folder='.')
 
 ppath = "./FolderToHost/"
 AutoIndex(app, browse_root=ppath)
