ae3dfe9e1e567d21dadad62e59cf6b602621d6be
Tory Jones <toryjjones2007@gmail.com>
2025-09-02 10:29:25 +1000

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_autoindex import AutoIndex
 
 app = Flask('Servicing', template_folder='.')
 
-ppath = "./FolderForHosting/"
+ppath = "./FolderToHost/"
 AutoIndex(app, browse_root=ppath)
 
 if __name__ == '__main__':
